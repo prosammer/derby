2407f3855f5ce0f211c1af0acedc26e3a8ebaec9
Prosammer <sam.finton@gmail.com>
2023-11-06 02:41:15 -0400

    Removed any swift-bridge references

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,12 +1,3 @@
-use std::path::Path;
-use swift_rs::SwiftLinker;
-
 fn main() {
-  const PACKAGE_NAME: &str = "swift_ocr";
-  const PACKAGE_PATH_STR: &str = "resources/swift_ocr";
-  SwiftLinker::new("11")
-      .with_package(PACKAGE_NAME, Path::new(PACKAGE_PATH_STR))
-      .link();
-
   tauri_build::build()
 }
--- src-tauri/src/screenshot.rs
+++ src-tauri/src/screenshot.rs
@@ -1,26 +1,18 @@
-use swift_rs::{SRArray, SRString, swift, SRObject};
-use anyhow::{Result, anyhow};
 use screenshots::Screen;
-
-swift!(fn screenshot_ocr() -> SRObject<RustResponse>);
-
-#[repr(C)]
-struct RustResponse {
-    success: bool,
-    content: SRArray<SRString>
-}
-
-pub fn ocr_screenshot() -> Result<Vec<String>> {
-    let rust_response = unsafe { screenshot_ocr() };
-
-    let mut response = Vec::new();
-    if rust_response.success {
-        for sr_string in rust_response.content.as_slice() {
-            response.push(sr_string.as_str().to_string());
-        }
-    } else {
-        return Err(anyhow!("Swift returned false"));
+#[tauri::command]
+pub fn ocr_screenshot() -> Result<Vec<String>, String> {
+    let screens = Screen::all().unwrap();
+    for screen in screens {
+        println!("capturer {screen:?}");
+        let mut image = screen.capture().unwrap();
+        image
+            .save(format!("target/{}.png", screen.display_info.id))
+            .unwrap();
     }
+
+    // TODO: OCR the screenshot
+    let mut response = Vec::new();
+    response.push("dummy text1".to_string());
 
     return Ok(response);
 }
