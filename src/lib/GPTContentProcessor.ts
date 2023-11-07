f006618e6b3c15fba888348f990248cf4f3717a8
Prosammer <sam.finton@gmail.com>
2023-11-06 23:46:03 -0400

    Fixed GPTContentProcessor.ts parsing

--- src/lib/GPTContentProcessor.ts
+++ src/lib/GPTContentProcessor.ts
@@ -1,31 +1,63 @@
+// GPTContentProcessor.ts
+
 export class GPTContentProcessor {
-  private static extractContent(jsonString: string): string {
-    try {
-      // Parse the input string as JSON
-      const data = JSON.parse(jsonString);
+  private buffer = '';
 
-      // Check if 'choices' and 'delta' fields exist
-      if (data.choices && data.choices[0] && data.choices[0].delta) {
-        // Return the 'content' if it exists, otherwise an empty string
-        return data.choices[0].delta.content || '';
+  private extractJsonObjects(buffer: string): string[] {
+    const jsonObjects = [];
+    let depth = 0;
+    let startIndex = -1;
+
+    // Iterate over the characters in the buffer to split complete JSON objects
+    for (let i = 0; i < buffer.length; i++) {
+      const char = buffer[i];
+      if (char === '{') {
+        // We have found the start of a JSON object
+        if (depth === 0) startIndex = i;
+        depth++;
+      } else if (char === '}') {
+        depth--;
+        // We have found the end of a JSON object
+        if (depth === 0 && startIndex !== -1) {
+          // Extract the complete JSON object and add it to the list
+          jsonObjects.push(buffer.substring(startIndex, i + 1));
+          startIndex = -1;
+        }
       }
-
-      return ''; // Return empty if content is not available or if the delta is empty
-    } catch (error) {
-      console.error('Failed to parse JSON or extract content', error);
-      return '';
     }
+
+    return jsonObjects;
   }
 
   public processChunk(chunk: string): string {
-    // Find the first occurrence of '{' to ensure we start parsing the JSON from the correct position
-    const jsonStartIndex = chunk.indexOf('{');
-    if (jsonStartIndex === -1) return '';  // Return empty if no JSON object start is found
+    let content = '';
 
-    // Extract JSON string starting from the first '{'
-    const jsonChunk = chunk.substring(jsonStartIndex);
+    // Add the new chunk to the buffer
+    this.buffer += chunk;
 
-    // Use the static method to extract content from the chunk
-    return GPTContentProcessor.extractContent(jsonChunk);
+    // Attempt to extract complete JSON objects from the buffer
+    const jsonObjects = this.extractJsonObjects(this.buffer);
+
+    jsonObjects.forEach(jsonObject => {
+      try {
+        // Parse the JSON object
+        const data = JSON.parse(jsonObject);
+
+        // Check if 'choices' and 'delta' fields exist and if 'content' is a string
+        if (data.choices?.[0]?.delta?.content) {
+          // If 'content' exists, append it to the output content string
+          content += data.choices[0].delta.content;
+        }
+      } catch (error) {
+        console.error('Failed to parse JSON object', error);
+        // If an error occurred, this JSON object is likely corrupted or incomplete
+      }
+    });
+
+    // Update the buffer with any remaining incomplete JSON
+    this.buffer = this.buffer.substring(this.buffer.lastIndexOf('}') + 1);
+
+    // Return any accumulated content from successfully parsed JSON objects
+    return content;
   }
 }
